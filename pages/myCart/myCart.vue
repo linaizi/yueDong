e700a8bdbc4483ebfc23de233686c1173c2a2f6b
linkunlin <linkunlin@hfhj178.com>
2023-09-04 21:24:12 +0800

    9/4

--- pages/myCart/myCart.vue
+++ pages/myCart/myCart.vue
@@ -1,6 +1,7 @@
 <template>
 	<view class="allBg flexBox">
 		 <view class="myCart-top">商品库存有限，请尽快下单  <p>编辑</p></view>
+		 
 		 <scroll-view scroll-y="true" lower-threshold="50"
 		 	@scrolltolower="scrollLower" 
 		 	@scroll='fromTop' 
@@ -12,22 +13,20 @@
 		 	@refresherrefresh="onRefresh"
 		 	@refresherrestore="onRestore"
 		 	@refresherabort="onAbort" >
-				
-				<view class="goodsList">
-					<view class="goodsItem" v-for="(item,index) in recommendArr" :key="index" >
-						<image :src="item.img" class="goodsImg"></image>
-						<view class="goodsMsg">
-							<view class="goodsMsg-top">
-								<h2 class="overflow2">{{item.name}}</h2>
-								<view class="goods-sal">已售：{{item.sales}} 件</view>
-							</view>
-							<view class="goodsMsg-foot">
-								<view class="foot-lf">
-										<p>￥<span class="big">{{item.newprice}}</span>{{item.newprice}}</p>
-										<p class="gray">￥{{item.oldprice}}</p>
+				<view class="myCartList">
+					<view class="myCartItem" v-for="(item,index) in recommendArr" :key="index" >
+						<view class="circle"></view>
+						<view class="itemRt">
+							<image :src="item.img" class="rt-Img"></image>
+							<view class="rt-mid">
+								<view class="mid-name overflow2">{{item.name}}</view>
+								<view class="mid-gz">规格：默认</view>
+								<view class="mid-price">
+									{{item.newprice}}
+									<uni-number-box :value="numberValue" @change="change" background="#fff" class="uniNum" />
 								</view>
-								<image class="foot-rf" src="../../static/img/gwc.jpg"></image>
 							</view>
+							
 						</view>
 					</view>
 				</view>
@@ -37,10 +36,19 @@
 				</view>
 		 </scroll-view>
 		 
-		 <!-- <view class="noGood" v-if="noGshow">
-		 	<image src="../../static/images/noGood.png" mode="widthFix" class="noGood-img"></image>
-		 	<p>没有找到相关的商品</p>
-		 </view> -->
+		 <view class="noGood" v-if="noGshow">
+				<image src="../../static/img/myCart1.jpg" mode="widthFix" class="noGood-img"></image>
+				<p>购物车还是空的哦</p>
+		 </view>
+		 
+		 <view class="myCart-foot">
+				<view class="foot-lt">
+					<view class="circle"></view>
+					<span class="lt-span">全选</span>
+					<span class="red">总计: ￥{{totalMoney}}</span>
+				</view>
+				<view class="foot-rt foot-rt-act">去结算</view>
+		 </view>
 		 
 		 <view class="goTop" @click="toTop" v-if="flag">
 		 		<image src="../../static/img/gotop.png" mode="aspectFit" class="goTop-img"></image>
@@ -64,6 +72,8 @@
 					{name:"中级鞋类精选2双", img:"../../static/index/logo.jpg",newprice:13.9,oldprice:49,sales:89,qg:2},
 					{name:"中级鞋类修复", img:"../../static/index/swiper3.png",newprice:13.9,oldprice:49,sales:89,qg:1},
 				],
+				noGshow:false,
+				totalMoney:0.00
 			}
 		},
 		onReady() {
