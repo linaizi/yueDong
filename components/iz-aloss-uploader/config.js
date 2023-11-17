e02328672351123b4a12c4c3b63f12595727e7e6
linkunlin <linkunlin@hfhj178.com>
2023-11-17 16:56:14 +0800

    1117x

--- components/iz-aloss-uploader/config.js
+++ components/iz-aloss-uploader/config.js
@@ -3,7 +3,7 @@ var config = {
   uploadImageUrl: 'https://www.yuedongxixie.com/elantra/img/file-upload',//阿里云上传域名
   AccessKeySecret: '***',//Secret，登录oss控制台查看
   OSSAccessKeyId: '***',//KeyId，登录oss控制台查看
-  timeout: 87600,
-  maxSize: 128 * 1024 * 1024 //上传文件的大小限制,128mb
+  timeout: 100000,
+  maxSize: 8 * 1024 * 1024 //上传文件的大小限制,8mb
 };
 module.exports = config
